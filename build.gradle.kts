3e1ed0a39528e1ab4fe74f96e722d3d25e924632
dEpayse <cbs5295@gmail.com>
2025-10-19 18:07:47 +0900

    build: Kotlin Logging 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Kotlin Logging
+    implementation("io.github.microutils:kotlin-logging:1.12.5")
 }
 
 kotlin {
